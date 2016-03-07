694790d4e1f58d1df1b7afd43990171c91d31a04
cfz23 <magicthegathering95@gmail.com>
2016-03-07 18:34:46 -0500

    tentative flip method added

--- src/coolchess/gui/Chessboard.java
+++ src/coolchess/gui/Chessboard.java
@@ -17,6 +17,7 @@ public class Chessboard {
 	private String[] starting = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"};
 	private Board b;
 	private boolean active = true;
+	private boolean player;
 	private boolean isViable;
 	private int activex;
 	private int activey;
@@ -33,6 +34,13 @@ public class Chessboard {
 		gui.add(options, BorderLayout.PAGE_START);
 		//add all the options
 		options.add(new JButton("Surrender"));
+		JButton flip = new JButton("Flip");
+		flip.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flipBoard();
+			}
+		});
+		options.add(flip);
 		board = new JPanel(new GridLayout(0,8));
 		board.setBorder(new LineBorder(Color.BLACK));
 		gui.add(board);
@@ -70,21 +78,20 @@ public class Chessboard {
 						else {
 							//check if current location is within the viable list
 							//if(is in viable list) {
-								
+							isViable = true;
 							//}
 							if(isViable) {
 								movePiece(activex, activey, xcord, ycord);
+								active = true;
 							}
 							else {
-								active = false;
+								active = true;
 								for(int k = 0; k < 10; k++) {
 									//change colors back to black and white through math
 								}
 							}
 						}
-					}
-
-					
+					}	
 				});
 				board.add(squares[i][j]);
 			}
@@ -98,7 +105,8 @@ public class Chessboard {
 		//System.out.println(j);
 		activex = i;
 		activey = j;
-		squares[i][j].setBackground(Color.RED);
+		squares[i][j].setBackground(Color.gray);
+		active = false;
 	}
 	
 	private void movePiece(int oldi, int oldj, int i, int j) {
@@ -106,6 +114,36 @@ public class Chessboard {
 		String temp = squares[oldi][oldj].getText();
 		squares[oldi][oldj].setText("");
 		squares[i][j].setText(temp);
+		//move in board as well
+	}
+	
+	private void flipBoard() {
+		JButton[][] temp = new JButton[8][8];
+		Insets margins = new Insets(0,0,0,0);
+		for(int i = 0; i < squares.length; i++) {
+			for (int j = 0; j < squares.length; j++) {
+				JButton button = new JButton();
+				button.setMargin(margins);
+				ImageIcon icon = new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+				button.setIcon(icon);
+				
+				if((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+					button.setBackground(Color.WHITE);
+				}
+				else {
+					button.setBackground(Color.BLACK);
+				}
+				
+				temp[7-i][7-j] = button;
+				
+				temp[7-i][7-j].setText(squares[i][j].getText());
+			}
+		}
+		for(int i = 0; i < temp.length; i++) {
+			for(int j = 0; j < temp.length; j++) {
+				squares[i][j].setText(temp[i][j].getText());
+			}
+		}
 	}
 	
 	private void setupBoard() {
