8d338bf88922c52b653c520a48882f8ac5a54cf0
Amndeep Singh Mann <amndeep.dev@gmail.com>
2016-03-10 15:58:06 -0500

    Made move a combination of pieces and result cells

--- src/coolchess/game/Move.java
+++ src/coolchess/game/Move.java
@@ -2,15 +2,30 @@ package coolchess.game;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Move implements Serializable {
 	private static final long serialVersionUID = 6068364453892314431L;
 	
 	private ArrayList<Piece> pieces;
+	private ArrayList<Cell> cells;
 	
-	public Move(Piece... pieces){
-		this.setPieces(new ArrayList<Piece>(Arrays.asList(pieces)));
+	// needs to be an arraylist of pieces and arraylist of cells that those pieces are going to go to
+	// add constructor for single piece
+	// contract implies that these are the same length
+	public Move(ArrayList<Piece> pieces, ArrayList<Cell> cells) throws InvalidMoveException{
+		if(pieces.size() != cells.size()){
+			throw new InvalidMoveException("Number of pieces and cells aren't equal");
+		}
+		
+		this.pieces = pieces;
+		this.setCells(cells);
+	}
+	
+	public Move(Piece piece, Cell cell){
+		pieces = new ArrayList<Piece>();
+		pieces.add(piece);
+		cells = new ArrayList<Cell>();
+		cells.add(cell);
 	}
 
 	public Move(Move m){
@@ -23,5 +38,33 @@ public class Move implements Serializable {
 
 	public void setPieces(ArrayList<Piece> pieces) {
 		this.pieces = pieces;
+	}
+	
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+	public void setCells(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
+
+	public static class InvalidMoveException extends Exception{
+		private static final long serialVersionUID = -2862426984521252427L;
+
+		public InvalidMoveException(){
+			super();
+		}
+
+		public InvalidMoveException(String message){
+			super(message);
+		}
+		
+		public InvalidMoveException(String message, Throwable cause){
+			super(message, cause);
+		}
+		
+		public InvalidMoveException(Throwable cause){
+			super(cause);
+		}
 	}
 }
