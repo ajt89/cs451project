b5cd95e745bb1c7568e54939be3f8c3156373381
Amndeep Singh Mann <amndeep.dev@gmail.com>
2016-03-09 20:45:03 -0500

    Shell for validator

--- src/coolchess/game/Manager.java
+++ src/coolchess/game/Manager.java
@@ -1,5 +1,7 @@
 package coolchess.game;
 
+import java.util.ArrayList;
+
 public class Manager {
 	private Board board;
 	
@@ -9,5 +11,43 @@ public class Manager {
 	
 	public void doMove(Move m){
 		
+	}
+	
+	public ArrayList<Cell> viableLocations(Cell c){
+		Piece p = board.getPiece(c);
+		
+		if(p == null){
+			return new ArrayList<Cell>();
+		}
+		else{
+			switch(p.type){
+			case BLACK_BISHOP:    
+				return Validator.bishopMoves(board, p, true);
+			case BLACK_KING:
+				return Validator.kingMoves(board, p, true);
+			case BLACK_KNIGHT:
+				return Validator.knightMoves(board, p, true);
+			case BLACK_PAWN:
+				return Validator.pawnMoves(board, p, true);
+			case BLACK_QUEEN:
+				return Validator.queenMoves(board, p, true);
+			case BLACK_ROOK:
+				return Validator.rookMoves(board, p, true);
+			case WHITE_BISHOP:
+				return Validator.bishopMoves(board, p, false);
+			case WHITE_KING:
+				return Validator.kingMoves(board, p, false);
+			case WHITE_KNIGHT:
+				return Validator.knightMoves(board, p, false);
+			case WHITE_PAWN:
+				return Validator.pawnMoves(board, p, false);
+			case WHITE_QUEEN:
+				return Validator.queenMoves(board, p, false);
+			case WHITE_ROOK:
+				return Validator.rookMoves(board, p, false);
+			default:
+				return null;
+			}
+		}
 	}
 }
--- src/coolchess/game/Validator.java
+++ src/coolchess/game/Validator.java
@@ -1,5 +1,29 @@
 package coolchess.game;
 
+import java.util.ArrayList;
+
 public class Validator {
-	
+	public static ArrayList<Cell> bishopMoves(Board board, Piece p, boolean isBlack) {
+		return new ArrayList<Cell>();
+	}
+
+	public static ArrayList<Cell> kingMoves(Board board, Piece p, boolean isBlack) {
+		return new ArrayList<Cell>();
+	}
+
+	public static ArrayList<Cell> knightMoves(Board board, Piece p, boolean isBlack) {
+		return new ArrayList<Cell>();
+	}
+
+	public static ArrayList<Cell> pawnMoves(Board board, Piece p, boolean isBlack) {
+		return new ArrayList<Cell>();
+	}
+
+	public static ArrayList<Cell> queenMoves(Board board, Piece p, boolean isBlack) {
+		return new ArrayList<Cell>();
+	}
+
+	public static ArrayList<Cell> rookMoves(Board board, Piece p, boolean isBlack) {
+		return new ArrayList<Cell>();
+	}
 }
