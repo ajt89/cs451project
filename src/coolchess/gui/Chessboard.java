893113c6ff46e21b486863ffa69063f0f9a15c0a
cfz23 <magicthegathering95@gmail.com>
2016-03-10 20:58:54 -0500

    included some print tests for client/server and start of junit

--- src/coolchess/gui/Chessboard.java
+++ src/coolchess/gui/Chessboard.java
@@ -141,6 +141,7 @@ public class Chessboard {
 						}
 						else {
 							//squares[xcord][ycord].setEnabled(false);
+							System.out.println("Does it get here on black?");
 							receiveMove(ch);
 							player = true;
 						}
@@ -259,6 +260,8 @@ public class Chessboard {
 		while(listening) {
 			try {
 				m = ch.getMove();
+				System.out.println(m.getCell().getNum());
+				System.out.println(m.getCell().getLet());
 				if(m != null) {
 					listening = false;
 				}
