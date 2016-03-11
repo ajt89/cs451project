82f0ce7f7a4e4469014837f9dca9cda973199790
Amndeep Singh Mann <amndeep.dev@gmail.com>
2016-03-10 22:01:00 -0500

    Should take care of the skewering situation when doing checks

--- src/coolchess/game/Validator.java
+++ src/coolchess/game/Validator.java
@@ -25,10 +25,18 @@ public class Validator {
 	public static ArrayList<Cell> removeChecks(Board board, Piece p, ArrayList<Cell> ret){
 		Cell start = p.getLoc();
 		for(Iterator<Cell> iter = ret.iterator(); iter.hasNext();){
-			p.setLoc(iter.next());
+			Cell c = iter.next();
+			Piece alreadythere = board.getPiece(c);
+			if(alreadythere != null){
+				alreadythere.setLoc(null);
+			}
+			p.setLoc(c);
 			// 0 is alright here since there's only 1 king per side ever
 			if(((King)(board.getPiecesOfTypes(p.getColor(), PieceTypes.Type.KING).get(0))).inCheck(board)){
 				iter.remove();
+			}
+			if(alreadythere != null){
+				alreadythere.setLoc(c);
 			}
 		}
 		p.setLoc(start);
@@ -39,38 +47,40 @@ public class Validator {
 	public static ArrayList<Cell> bishopMoves(Board board, Bishop p) {
 		ArrayList<Cell> ret = new ArrayList<Cell>();
 		
-		int n = p.getLoc().getNum();
-		int l = p.getLoc().getLet();
-		for(int i = 1; n + i < Board.boardSize && l + i < Board.boardSize; i++){
-			Cell c = new Cell(n + i, l + i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+		if(p != null){
+			int n = p.getLoc().getNum();
+			int l = p.getLoc().getLet();
+			for(int i = 1; n + i < Board.boardSize && l + i < Board.boardSize; i++){
+				Cell c = new Cell(n + i, l + i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n + i < Board.boardSize && l - i >= 0; i++){
-			Cell c = new Cell(n + i, l - i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n + i < Board.boardSize && l - i >= 0; i++){
+				Cell c = new Cell(n + i, l - i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n - i >= 0 && l + i < Board.boardSize; i++){
-			Cell c = new Cell(n - i, l + i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n - i >= 0 && l + i < Board.boardSize; i++){
+				Cell c = new Cell(n - i, l + i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n - i >= 0 && l - i >= 0; i++){
-			Cell c = new Cell(n - i, l - i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n - i >= 0 && l - i >= 0; i++){
+				Cell c = new Cell(n - i, l - i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
+			
+			removeSameSide(board, p, ret);
 		}
-		
-		removeSameSide(board, p, ret);
 		
 		return ret;
 	}
@@ -78,64 +88,66 @@ public class Validator {
 	public static ArrayList<Cell> kingMoves(Board board, King p) {
 		ArrayList<Cell> ret = new ArrayList<Cell>();
 		
-		int n = p.getLoc().getNum();
-		int l = p.getLoc().getLet();
-		if(n + 1 < Board.boardSize && l + 1 < Board.boardSize){
-			ret.add(new Cell(n + 1, l + 1));
-		}
-		if(n + 1 < Board.boardSize && l - 1 >= 0){
-			ret.add(new Cell(n + 1, l - 1));
-		}
-		if(n - 1 >= 0 && l + 1 < Board.boardSize){
-			ret.add(new Cell(n - 1, l + 1));
-		}
-		if(n - 1 >= 0 && l - 1 >= 0){
-			ret.add(new Cell(n - 1, l - 1));
-		}
-		if(n + 1 < Board.boardSize){
-			ret.add(new Cell(n + 1, l));
-		}
-		if(n - 1 >= 0){
-			ret.add(new Cell(n - 1, l));
-		}
-		if(l + 1 < Board.boardSize){
-			ret.add(new Cell(n, l + 1));
-		}
-		if(l - 1 >= 0){
-			ret.add(new Cell(n, l - 1));
-		}
-		
-		//castling
-		if(!p.hasMoved() && !p.inCheck(board)){
-			for(Piece r : board.getPiecesOfTypes(p.getColor(), PieceTypes.Type.ROOK)){
-				Rook rr = (Rook) r;
-				if(!rr.hasMoved()){
-					boolean occupied = false;
-					for(int i = 1 + Math.min(p.getLoc().getLet(), rr.getLoc().getLet()); i < Math.max(p.getLoc().getLet(), rr.getLoc().getLet()); i++){
-						if(board.getPiece(new Cell(p.getLoc().getNum(), i)) != null){
-							occupied = true;
-							break;
-						}
-					}
-					if(!occupied){
-						if(rr.getLoc().getLet() == Math.min(p.getLoc().getLet(), rr.getLoc().getLet())){
-							Cell left = new Cell(p.getLoc().getNum(), p.getLoc().getLet()-2);
-							if(!p.testCheck(board, left)){
-								ret.add(left);
+		if(p != null){
+			int n = p.getLoc().getNum();
+			int l = p.getLoc().getLet();
+			if(n + 1 < Board.boardSize && l + 1 < Board.boardSize){
+				ret.add(new Cell(n + 1, l + 1));
+			}
+			if(n + 1 < Board.boardSize && l - 1 >= 0){
+				ret.add(new Cell(n + 1, l - 1));
+			}
+			if(n - 1 >= 0 && l + 1 < Board.boardSize){
+				ret.add(new Cell(n - 1, l + 1));
+			}
+			if(n - 1 >= 0 && l - 1 >= 0){
+				ret.add(new Cell(n - 1, l - 1));
+			}
+			if(n + 1 < Board.boardSize){
+				ret.add(new Cell(n + 1, l));
+			}
+			if(n - 1 >= 0){
+				ret.add(new Cell(n - 1, l));
+			}
+			if(l + 1 < Board.boardSize){
+				ret.add(new Cell(n, l + 1));
+			}
+			if(l - 1 >= 0){
+				ret.add(new Cell(n, l - 1));
+			}
+			
+			//castling
+			if(!p.hasMoved() && !p.inCheck(board)){
+				for(Piece r : board.getPiecesOfTypes(p.getColor(), PieceTypes.Type.ROOK)){
+					Rook rr = (Rook) r;
+					if(!rr.hasMoved()){
+						boolean occupied = false;
+						for(int i = 1 + Math.min(p.getLoc().getLet(), rr.getLoc().getLet()); i < Math.max(p.getLoc().getLet(), rr.getLoc().getLet()); i++){
+							if(board.getPiece(new Cell(p.getLoc().getNum(), i)) != null){
+								occupied = true;
+								break;
 							}
 						}
-						else{
-							Cell right = new Cell(p.getLoc().getNum(), p.getLoc().getLet()+2);
-							if(!p.testCheck(board, right)){
-								ret.add(right);
+						if(!occupied){
+							if(rr.getLoc().getLet() == Math.min(p.getLoc().getLet(), rr.getLoc().getLet())){
+								Cell left = new Cell(p.getLoc().getNum(), p.getLoc().getLet()-2);
+								if(!p.testCheck(board, left)){
+									ret.add(left);
+								}
+							}
+							else{
+								Cell right = new Cell(p.getLoc().getNum(), p.getLoc().getLet()+2);
+								if(!p.testCheck(board, right)){
+									ret.add(right);
+								}
 							}
 						}
 					}
 				}
 			}
+			
+			removeSameSide(board, p, ret);
 		}
-		
-		removeSameSide(board, p, ret);
 		
 		return ret;
 	}
@@ -143,20 +155,22 @@ public class Validator {
 	public static ArrayList<Cell> knightMoves(Board board, Knight p) {
 		ArrayList<Cell> ret = new ArrayList<Cell>();
 		
-		int n = p.getLoc().getNum();
-		int l = p.getLoc().getLet();
-		for(int r = -1; r <= 1; r+=2){
-			for(int c = -1; c <= 1; c+=2){
-				if(n + 1*r >= 0 && l + 2*c >= 0 && n + 1*r < Board.boardSize && l + 2*c < Board.boardSize){
-					ret.add(new Cell(n + 1*r, l + 2*c));
-				}
-				if(n + 2*r >= 0 && l + 1*c >= 0 && n + 2*r < Board.boardSize && l + 1*c < Board.boardSize){
-					ret.add(new Cell(n + 2*r, l + 1*c));
+		if(p != null){
+			int n = p.getLoc().getNum();
+			int l = p.getLoc().getLet();
+			for(int r = -1; r <= 1; r+=2){
+				for(int c = -1; c <= 1; c+=2){
+					if(n + 1*r >= 0 && l + 2*c >= 0 && n + 1*r < Board.boardSize && l + 2*c < Board.boardSize){
+						ret.add(new Cell(n + 1*r, l + 2*c));
+					}
+					if(n + 2*r >= 0 && l + 1*c >= 0 && n + 2*r < Board.boardSize && l + 1*c < Board.boardSize){
+						ret.add(new Cell(n + 2*r, l + 1*c));
+					}
 				}
 			}
+			
+			removeSameSide(board, p, ret);
 		}
-		
-		removeSameSide(board, p, ret);
 		
 		return ret;
 	}
@@ -164,44 +178,46 @@ public class Validator {
 	public static ArrayList<Cell> pawnMoves(Board board, Pawn p) {
 		ArrayList<Cell> ret = new ArrayList<Cell>();
 		
-		int n = p.getLoc().getNum();
-		int l = p.getLoc().getLet();
-		if(p.getColor() == PieceTypes.Color.BLACK){
-			if(n + 1 < Board.boardSize){
-				ret.add(new Cell(n + 1, l));
-				if(l + 1 < Board.boardSize){
-					Cell c = new Cell(n + 1, l + 1);
-					if(board.getPiece(c) != null){
-						ret.add(c);
+		if(p != null){
+			int n = p.getLoc().getNum();
+			int l = p.getLoc().getLet();
+			if(p.getColor() == PieceTypes.Color.BLACK){
+				if(n + 1 < Board.boardSize){
+					ret.add(new Cell(n + 1, l));
+					if(l + 1 < Board.boardSize){
+						Cell c = new Cell(n + 1, l + 1);
+						if(board.getPiece(c) != null){
+							ret.add(c);
+						}
 					}
-				}
-				if(l - 1 >= 0){
-					Cell c = new Cell(n + 1, l - 1);
-					if(board.getPiece(c) != null){
-						ret.add(c);
-					}
-				}
-			}
-		}
-		else{
-			if(n - 1 >= 0){
-				ret.add(new Cell(n - 1, l));
-				if(l + 1 < Board.boardSize){
-					Cell c = new Cell(n - 1, l + 1);
-					if(board.getPiece(c) != null){
-						ret.add(c);
-					}
-				}
-				if(l - 1 >= 0){
-					Cell c = new Cell(n - 1, l - 1);
-					if(board.getPiece(c) != null){
-						ret.add(c);
+					if(l - 1 >= 0){
+						Cell c = new Cell(n + 1, l - 1);
+						if(board.getPiece(c) != null){
+							ret.add(c);
+						}
 					}
 				}
 			}
+			else{
+				if(n - 1 >= 0){
+					ret.add(new Cell(n - 1, l));
+					if(l + 1 < Board.boardSize){
+						Cell c = new Cell(n - 1, l + 1);
+						if(board.getPiece(c) != null){
+							ret.add(c);
+						}
+					}
+					if(l - 1 >= 0){
+						Cell c = new Cell(n - 1, l - 1);
+						if(board.getPiece(c) != null){
+							ret.add(c);
+						}
+					}
+				}
+			}
+			
+			removeSameSide(board, p, ret);
 		}
-		
-		removeSameSide(board, p, ret);
 		
 		return ret;
 	}
@@ -209,66 +225,68 @@ public class Validator {
 	public static ArrayList<Cell> queenMoves(Board board, Queen p) {
 		ArrayList<Cell> ret = new ArrayList<Cell>();
 		
-		int n = p.getLoc().getNum();
-		int l = p.getLoc().getLet();
-		for(int i = 1; n + i < Board.boardSize && l + i < Board.boardSize; i++){
-			Cell c = new Cell(n + i, l + i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+		if(p != null){
+			int n = p.getLoc().getNum();
+			int l = p.getLoc().getLet();
+			for(int i = 1; n + i < Board.boardSize && l + i < Board.boardSize; i++){
+				Cell c = new Cell(n + i, l + i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n + i < Board.boardSize && l - i >= 0; i++){
-			Cell c = new Cell(n + i, l - i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n + i < Board.boardSize && l - i >= 0; i++){
+				Cell c = new Cell(n + i, l - i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n - i >= 0 && l + i < Board.boardSize; i++){
-			Cell c = new Cell(n - i, l + i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n - i >= 0 && l + i < Board.boardSize; i++){
+				Cell c = new Cell(n - i, l + i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n - i >= 0 && l - i >= 0; i++){
-			Cell c = new Cell(n - i, l - i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n - i >= 0 && l - i >= 0; i++){
+				Cell c = new Cell(n - i, l - i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n + i < Board.boardSize; i++){
-			Cell c = new Cell(n + i, l);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n + i < Board.boardSize; i++){
+				Cell c = new Cell(n + i, l);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n - i >= 0; i++){
-			Cell c = new Cell(n - i, l);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n - i >= 0; i++){
+				Cell c = new Cell(n - i, l);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; l + i < Board.boardSize; i++){
-			Cell c = new Cell(n, l + i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; l + i < Board.boardSize; i++){
+				Cell c = new Cell(n, l + i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; l - i >= 0; i++){
-			Cell c = new Cell(n, l - i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; l - i >= 0; i++){
+				Cell c = new Cell(n, l - i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
+			
+			removeSameSide(board, p, ret);
 		}
-		
-		removeSameSide(board, p, ret);
 		
 		return ret;
 	}
@@ -276,39 +294,41 @@ public class Validator {
 	public static ArrayList<Cell> rookMoves(Board board, Rook p) {
 		ArrayList<Cell> ret = new ArrayList<Cell>();
 		
-		int n = p.getLoc().getNum();
-		int l = p.getLoc().getLet();
-
-		for(int i = 1; n + i < Board.boardSize; i++){
-			Cell c = new Cell(n + i, l);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+		if(p != null){
+			int n = p.getLoc().getNum();
+			int l = p.getLoc().getLet();
+	
+			for(int i = 1; n + i < Board.boardSize; i++){
+				Cell c = new Cell(n + i, l);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; n - i >= 0; i++){
-			Cell c = new Cell(n - i, l);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; n - i >= 0; i++){
+				Cell c = new Cell(n - i, l);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; l + i < Board.boardSize; i++){
-			Cell c = new Cell(n, l + i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; l + i < Board.boardSize; i++){
+				Cell c = new Cell(n, l + i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
-		}
-		for(int i = 1; l - i >= 0; i++){
-			Cell c = new Cell(n, l - i);
-			ret.add(c);
-			if(board.getPiece(c) != null){
-				break;
+			for(int i = 1; l - i >= 0; i++){
+				Cell c = new Cell(n, l - i);
+				ret.add(c);
+				if(board.getPiece(c) != null){
+					break;
+				}
 			}
+			
+			removeSameSide(board, p, ret);
 		}
-		
-		removeSameSide(board, p, ret);
 		
 		return ret;
 	}
